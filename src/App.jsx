d89d39c5d2891cb02aaec50b96c0593ecbfbf109
Tavian Dodd <taviand35@gmail.com>
2026-01-20 20:18:42 -0600

    Init commit w/ .glb and camera controls for model

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+{/*
+Filename: App.jsx
+Author: Tavian Dodd
+Date Created: 01/15/2026
+Last Updated: 01/20/2026
+*/}
+
+import { useState, useRef, useEffect } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Environment, SpotLight, CameraControls } from '@react-three/drei'
+import { Model } from './Room'
+
+const standingPos = [1.7, 1.75, 2.4]
+const standingTarget = [0.25, 1, 0]
+const sittingPos = [1.1, 1.1, 0]
+const sittingTarget = [0, 0.85, 0]
+
+function Controls({ isSitting }) {
+  const controls = useRef()
+
+  // Toggle Animation
+  useEffect(() => {
+    if (controls.current) {
+      if (isSitting) {
+        controls.current.setLookAt(...sittingPos, ...sittingTarget, true)
+      } else {
+        controls.current.setLookAt(...standingPos, ...standingTarget, true)
+      }
+    }
+  }, [isSitting])
+
+  // Initial Snap
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (controls.current) {
+        controls.current.setLookAt(...standingPos, ...standingTarget, false)
+      }
+    }, 25)
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <CameraControls 
+      ref={controls} 
+      makeDefault 
+      smoothTime={1.5}
+      mouseButtons={{ left: 0, middle: 0, right: 0, wheel: 0 }}
+      touches={{ one: 0, two: 0, three: 0 }}
+    />
+  )
+}
+
+export default function App() {
+  const [isSitting, setIsSitting] = useState(false)
+
+  return (
+    <div style={{ width: "100vw", height: "100vh", background: "#111" }}>
+
+      <div style={{position: 'absolute', top: 20, left: 20, zIndex: 10}}>
+        <button
+          // Click Handler
+          onClick={() => setIsSitting(!isSitting)} 
+          style={{padding: '10px 20px', fontSize: '1.2rem', cursor: 'pointer'}}
+        >
+          {isSitting ? "Back To Room" : "View Work"}
+        </button>
+      </div>
+
+      <Canvas camera={{ position: standingPos, fov: 50 }}>
+        
+        <ambientLight intensity={0.35} />
+        <Environment preset="city" background={false} blur={1} environmentIntensity={0.2} />
+
+        <rectAreaLight
+          width={10}
+          height={10}
+          position={[0, 5, -5]}
+          intensity={5}
+          color="#2c2c54"
+          lookAt={[0, 0, 0]}
+        />
+
+        <SpotLight 
+          position={[3, 4, 2]} 
+          angle={0.5} 
+          penumbra={0.5} 
+          intensity={2} 
+          castShadow 
+          color="#e0e0ff" 
+        />
+
+        <SpotLight 
+          position={[-2, 3, -2]} 
+          intensity={3} 
+          color="blue" 
+          angle={0.5} 
+        />
+
+        <Model />
+
+        <Controls isSitting={isSitting} />
+        
+      </Canvas>
+    </div>
+  )
+}
+
+
